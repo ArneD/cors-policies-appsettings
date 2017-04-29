6499c770ad61d9cc53352689c4d99d7337faf6ca
Arne Dumarey <dumarey.arne+github@gmail.com>
2017-04-29 21:17:44 +0200

    Added Sample for a 'Test' policy

--- src/Samples/CorsPolicySettings.Sample/Program.cs
+++ src/Samples/CorsPolicySettings.Sample/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
 namespace CorsPolicySettings.Sample
@@ -17,7 +12,6 @@ namespace CorsPolicySettings.Sample
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
--- src/Samples/CorsPolicySettings.Sample/Startup.cs
+++ src/Samples/CorsPolicySettings.Sample/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +23,8 @@ namespace CorsPolicySettings.Sample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCorsPolicies(Configuration);
+
             // Add framework services.
             services.AddMvc();
         }
@@ -37,6 +35,7 @@ namespace CorsPolicySettings.Sample
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            app.UseCors("Test");
             app.UseMvc();
         }
     }
